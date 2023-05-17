9fa250eef2f57f402ecab51fbbc91dfaaddb2830
Daria <destroyapoison@yandex.ru>
2023-05-17 12:18:43 +0300

    code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿string[] inputArray = new string[] { "apple", "banana", "car", "dog", "elephant", "fox" };
+int newArraySize = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        newArraySize++;
+    }
+}
+string[] newArray = new string[newArraySize];
+int index = 0;
+for (int i = 0; i < inputArray.Length; i++)
+{
+    if (inputArray[i].Length <= 3)
+    {
+        newArray[index] = inputArray[i];
+        index++;
+    }
+}
+Console.WriteLine("New array:");
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine(newArray[i]);
+}
